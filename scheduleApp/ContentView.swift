1d144cc1f1793a2ea789508a253bc465748e5962
enriquemelgar <enrique.a.melgar@gmail.com>
2026-02-21 23:10:42 +0100

    Schedule App on MacOS initial setup

--- scheduleApp/ContentView.swift
+++ scheduleApp/ContentView.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            ForEach(topics) { topic in
+                Button(action: showBooksList) {
+                    Label(topic.name, systemImage: topic.icon)
+                }
+            }
+            Spacer()
+        }.padding(10)
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
@@ -17,6 +25,22 @@ struct ContentView: View {
         }
         .padding()
     }
+}
+
+struct Topics: Identifiable {
+    let id = UUID()
+    var name: String
+    var icon: String
+}
+
+var topics = [
+    Topics(name: "Books", icon: "book"),
+    Topics(name: "Movies/Series", icon: "movieclapper"),
+    Topics(name: "Games", icon: "gamecontroller")
+]
+
+private func showBooksList() {
+    
 }
 
 #Preview {
